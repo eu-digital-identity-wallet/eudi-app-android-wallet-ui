b3ca385ae9c7487c2cf47c2304ebd2b3d9e89dd0
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany-intrasoft.com>
2023-11-29 18:15:58 +0200

    forgot to remove firebase plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.gms) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secrets) apply false
