eaa2a5d79dc8f87f3dc6d38a3897a2c0b1f5ebdd
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany.com>
2024-02-06 11:51:10 +0200

    New issuer URL

--- business-logic/src/main/java/eu/europa/ec/businesslogic/config/ConfigWalletCoreImpl.kt
+++ business-logic/src/main/java/eu/europa/ec/businesslogic/config/ConfigWalletCoreImpl.kt
@@ -30,7 +30,7 @@ internal class WalletCoreConfigImpl(
     companion object {
         const val VERIFIER_API_URI = "https://dev.verifier.eudiw.dev"
         const val VERIFIER_CLIENT_ID = "Verifier"
-        const val VCI_ISSUER_URL = "https://preprod.issuer.eudiw.dev/oidc"
+        const val VCI_ISSUER_URL = "https://issuer.eudiw.dev/oidc"
         const val VCI_CLIENT_ID = "wallet-dev"
     }
 
