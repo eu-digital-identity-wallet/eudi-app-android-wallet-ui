e836f6e6e255c95e3a65e5f5a5dfd6d196802220
Stilianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2024-06-03 11:06:44 +0300

    new vp/vci ids

--- core-logic/src/demo/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
+++ core-logic/src/demo/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
@@ -32,9 +32,9 @@ internal class WalletCoreConfigImpl(
     private companion object {
         const val OPENID4VP_VERIFIER_API_URI = "https://verifier.eudiw.dev"
         const val OPENID4VP_VERIFIER_LEGAL_NAME = "EUDI Remote Verifier"
-        const val OPENID4VP_VERIFIER_CLIENT_ID = "Verifier"
+        const val OPENID4VP_VERIFIER_CLIENT_ID = "eudi-verifier-demo"
         const val VCI_ISSUER_URL = "https://issuer.eudiw.dev/oidc"
-        const val VCI_CLIENT_ID = "wallet-demo"
+        const val VCI_CLIENT_ID = "eudi-vci-demo"
         const val AUTHENTICATION_REQUIRED = false
     }
 
