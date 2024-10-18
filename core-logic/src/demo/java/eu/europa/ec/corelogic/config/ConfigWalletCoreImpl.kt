654efdd72b10f878bd5b8f649d53211d859e3491
Sigvald Høyheim <sigvald.hoyheim@digdir.no>
2024-10-18 15:07:58 +0200

    legg til digdir ikoner i demo mode

--- core-logic/src/demo/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
+++ core-logic/src/demo/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
@@ -45,6 +45,11 @@ internal class WalletCoreConfigImpl(
 ) : WalletCoreConfig {
 
     private companion object {
+        /*
+        const val VCI_ISSUER_URL = "https://abr.vc.local"
+        const val VCI_CLIENT_ID = "abr.vc.local"
+        const val AUTHENTICATION_REQUIRED = false
+        */
         const val VCI_ISSUER_URL = "https://demo-vc-issuer.idporten.dev"
         const val VCI_CLIENT_ID = "demo-vc-issuer.idporten.dev"
         const val AUTHENTICATION_REQUIRED = false
