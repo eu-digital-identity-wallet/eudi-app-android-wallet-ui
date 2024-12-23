17719633484143dc580d81979e44108058a68c2d
STYLIATIS Stamatis <stamatis.styliatis@netcompany.com>
2024-12-23 14:34:24 +0200

    Added Content Screens and TopBar as a backbone for further implementation

--- dashboard-feature/src/main/java/eu/europa/ec/dashboardfeature/ui/home/HomeScreen.kt
+++ dashboard-feature/src/main/java/eu/europa/ec/dashboardfeature/ui/home/HomeScreen.kt
@@ -17,20 +17,84 @@
 package eu.europa.ec.dashboardfeature.ui.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import eu.europa.ec.dashboardfeature.interactor.HomeInteractorImpl
+import eu.europa.ec.resourceslogic.R
+import eu.europa.ec.uilogic.component.AppIcons
+import eu.europa.ec.uilogic.component.content.ContentScreen
+import eu.europa.ec.uilogic.component.content.ScreenNavigateAction
+import eu.europa.ec.uilogic.component.preview.ThemeModePreviews
+import eu.europa.ec.uilogic.component.utils.SIZE_XX_LARGE
+import eu.europa.ec.uilogic.component.utils.SPACING_MEDIUM
+import eu.europa.ec.uilogic.component.wrap.WrapIcon
 
 @Composable
-fun HomeScreen(navHostController: NavController, homeViewModel: HomeViewModel) {
+fun HomeScreen(navHostController: NavController, viewModel: HomeViewModel) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        ContentScreen(
+            isLoading = false,
+            navigatableAction = ScreenNavigateAction.NONE,
+            topBar = { TopBar() }
+        ) {
+            Content()
+        }
+    }
+}
+
+@Composable
+private fun TopBar() {
+    Row(
+        modifier = Modifier
+            .height(SIZE_XX_LARGE.dp)
+            .fillMaxSize()
+            .padding(SPACING_MEDIUM.dp),
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.headlineLarge,
+            text = stringResource(R.string.home_screen_title)
+        )
+        WrapIcon(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            iconData = AppIcons.Add
+        )
+    }
+}
+
+@Composable
+private fun Content() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
         Text("Home")
     }
+}
 
+@ThemeModePreviews
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen(
+        rememberNavController(),
+        viewModel = HomeViewModel(HomeInteractorImpl())
+    )
 }
--- dashboard-feature/src/main/java/eu/europa/ec/dashboardfeature/ui/transactions/TransactionsScreen.kt
+++ dashboard-feature/src/main/java/eu/europa/ec/dashboardfeature/ui/transactions/TransactionsScreen.kt
@@ -17,22 +17,87 @@
 package eu.europa.ec.dashboardfeature.ui.transactions
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import eu.europa.ec.dashboardfeature.interactor.TransactionsInteractorImpl
+import eu.europa.ec.resourceslogic.R
+import eu.europa.ec.uilogic.component.AppIcons
+import eu.europa.ec.uilogic.component.content.ContentScreen
+import eu.europa.ec.uilogic.component.content.ScreenNavigateAction
+import eu.europa.ec.uilogic.component.preview.ThemeModePreviews
+import eu.europa.ec.uilogic.component.utils.SIZE_XX_LARGE
+import eu.europa.ec.uilogic.component.utils.SPACING_MEDIUM
+import eu.europa.ec.uilogic.component.wrap.WrapIcon
 
 @Composable
 fun TransactionsScreen(
     navHostController: NavController,
-    transactionsViewModel: TransactionsViewModel,
+    viewModel: TransactionsViewModel,
 ) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        ContentScreen(
+            isLoading = false,
+            navigatableAction = ScreenNavigateAction.NONE,
+            topBar = { TopBar() }
+        ) {
+            Content()
+        }
+    }
+}
+
+@Composable
+private fun TopBar() {
+    Row(
+        modifier = Modifier
+            .height(SIZE_XX_LARGE.dp)
+            .fillMaxSize()
+            .padding(SPACING_MEDIUM.dp),
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.headlineLarge,
+            text = stringResource(R.string.transactions_screen_title)
+        )
+        WrapIcon(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            iconData = AppIcons.Add
+        )
+    }
+}
+
+@Composable
+private fun Content() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
         Text("Transactions")
     }
+}
+
+@ThemeModePreviews
+@Composable
+private fun TransactionsScreenPreview() {
+    TransactionsScreen(
+        rememberNavController(),
+        viewModel = TransactionsViewModel(TransactionsInteractorImpl())
+    )
 }
