509563b377687528f342368e11418b9c678dc47c
Stilianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2025-04-11 02:54:01 +0300

    Refactor: Clarify and enhance PrefsController documentation
    Improved the documentation for the PrefsController interface and its implementation, providing clearer explanations of its functionalities and usage, including data storage and retrieval operations.
    Removed redundant comments.

--- business-logic/src/main/java/eu/europa/ec/businesslogic/controller/storage/PrefsController.kt
+++ business-logic/src/main/java/eu/europa/ec/businesslogic/controller/storage/PrefsController.kt
@@ -52,9 +52,6 @@ interface PrefsController {
      * Assigns given [value] to device storage - shared preferences given [key]. You can
      * retrieve this value by calling [getString].
      *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
-     *
      * @param key   Key used to add given [value].
      * @param value Value to add after given [key].
      */
@@ -63,9 +60,6 @@ interface PrefsController {
     /**
      * Assigns given [value] to device storage - shared preferences given [key]. You can
      * retrieve this value by calling [getString].
-     *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
      *
      * @param key   Key used to add given [value].
      * @param value Value to add after given [key].
@@ -78,9 +72,6 @@ interface PrefsController {
      * Assigns given [value] to device storage - shared preferences given [key]. You can
      * retrieve this value by calling [getString].
      *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
-     *
      * @param key   Key used to add given [value].
      * @param value Value to add after given [key].
      */
@@ -89,9 +80,6 @@ interface PrefsController {
     /**
      * Retrieves a string value from device shared preferences that corresponds to given [key]. If
      * key does not exist or value of given key is null, [defaultValue] is returned.
-     *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
      *
      * @param key          Key to get corresponding value.
      * @param defaultValue Default value to return if given [key] does not exist in prefs or if
@@ -103,9 +91,6 @@ interface PrefsController {
      * Retrieves a long value from device shared preferences that corresponds to given [key]. If
      * key does not exist or value of given key is null, [defaultValue] is returned.
      *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
-     *
      * @param key          Key to get corresponding value.
      * @param defaultValue Default value to return if given [key] does not exist in prefs or if
      * key value is invalid.
@@ -113,35 +98,62 @@ interface PrefsController {
     fun getLong(key: String, defaultValue: Long): Long
 
     /**
-     * Retrieves a boolean value from device shared preferences that corresponds to given [key]. If
-     * key does not exist or value of given key is null, [defaultValue] is returned.
+     * Retrieves a boolean value from the device's shared preferences associated with the given [key].
      *
-     * Shared preferences are encrypted. Do not create your own instance to add or retrieve data.
-     * Instead, call operations of this controller.
+     * If the [key] is not found in the preferences, or if the value associated with the [key] is null,
+     * the [defaultValue] is returned.  Note that if a value exists for the key but is not a valid
+     * boolean (e.g., a String or an Int), the platform may also return the [defaultValue], depending on
+     * the underlying shared preferences implementation.
      *
-     * @param key          Key to get corresponding value.
-     * @param defaultValue Default value to return if given [key] does not exist in prefs or if
-     * key value is invalid.
+     * @param key The key used to retrieve the boolean value.
+     * @param defaultValue The boolean value to return if the [key] is not found or has a null value.
+     * @return The boolean value associated with the [key], or the [defaultValue] if the [key] is not found or has a null value.
      */
     fun getBool(key: String, defaultValue: Boolean): Boolean
-    fun setInt(key: String, value: Int)
-    fun getInt(key: String, defaultValue: Int): Int
 
+    /**
+     * Sets an integer value associated with the given key in the underlying data store.
+     * If a value already exists for the key, it will be overwritten.
+     *
+     * @param key The unique identifier for the integer value.  Must not be null or empty.
+     * @param value The integer value to store.
+     */
+    fun setInt(key: String, value: Int)
+
+    /**
+     * Retrieves an integer value associated with the given key from a data source.
+     * If the key is not found or the value is not an integer, it returns the specified default value.
+     *
+     * @param key The key associated with the integer value to retrieve.
+     * @param defaultValue The default integer value to return if the key is not found or the value is not an integer.
+     * @return The integer value associated with the key, or the default value if the key is not found or the value is invalid.
+     */
+    fun getInt(key: String, defaultValue: Int): Int
 }
 
 /**
- * Controller used to manipulate data stored in device [SharedPreferences]. Data are encrypted so
- * you are strongly advised to used this controller to set or get values.
+ * Implementation of the [PrefsController] interface for managing application preferences.
+ *
+ * This class provides methods for storing and retrieving various data types (String, Long, Boolean, Int)
+ * in the application's SharedPreferences.  All SharedPreferences are
+ * stored within a file named "eudi-wallet" accessible only to this application.
+ *
+ * @property resourceProvider An instance of [ResourceProvider] used to access application resources,
+ *                           including the application context for obtaining SharedPreferences.
  */
 class PrefsControllerImpl(
     private val resourceProvider: ResourceProvider
 ) : PrefsController {
 
+
     /**
-     * Initializes and returns a new [SharedPreferences] instance. Instance is using an encryption
-     * to store data in device.
+     * Retrieves the SharedPreferences instance for the application.
      *
-     * @return A new [SharedPreferences] instance.
+     * This function provides access to the SharedPreferences object used by the application
+     * for persistent storage of key-value pairs. The SharedPreferences are named "eudi-wallet"
+     * and are accessed with private mode, meaning only this application can read or write to them.
+     *
+     * @return The SharedPreferences instance.
      */
     private fun getSharedPrefs(): SharedPreferences {
         return resourceProvider.provideContext().getSharedPreferences("eudi-wallet", MODE_PRIVATE)
